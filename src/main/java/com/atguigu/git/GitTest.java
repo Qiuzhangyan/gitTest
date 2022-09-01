7ff6737107cddb5c66eb3fe1f18cec0838471bfe
Jarvis <qiuzz@163.com>
2022-09-01 11:15:09 +0800

    Firest commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Jarviz
+ * @date 2022/9/1 - 11:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!!");
+    }
+}
